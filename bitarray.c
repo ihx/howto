f8be7e540a0787be873f1fe591baf37b424bd562
Matthew Temple <clownfysh@gmail.com>
2012-04-13 12:56:21 +0000

    worked on genetic and tube systems

--- bitarray.c
+++ bitarray.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[])
   ih_bitarray_t bitarray;
   unsigned char value;
 
-  ih_bitarray_set_uchar(&bitarray, 0, 117);
-  value = ih_bitarray_get_uchar(&bitarray, 0);
+  ih_bitarray_set_uint8(&bitarray, 0, 117);
+  value = ih_bitarray_get_uint8(&bitarray, 0);
 
   printf("%d\n", value);
 
---
+++ genetic.c
@@ -0,0 +1,69 @@
+#include "bit.h"
+#include "boole.h"
+#include "genetic.h"
+#include "standard.h"
+#include "trace.h"
+
+#define GOAL_TOTAL 1024
+
+unsigned short numbers[IH_GENOME_SIZE_BITS] = {
+  23, 123, 456, 235, 567, 123, 11, 89,
+  213, 1, 76, 57, 78, 131, 411, 859,
+  44, 45, 56, 315, 15, 126, 101, 889,
+  243, 423, 9356, 987, 192, 348, 45, 54,
+  123, 2123, 3456, 4235, 55676, 1723, 118, 809,
+  1213, 21, 763, 574, 785, 1316, 4711, 8589,
+  144, 245, 563, 3154, 515, 1266, 1701, 8889,
+  1243, 2423, 39356, 49875, 6192, 3478, 485, 954
+};
+
+double calculate_fitness(ih_genome_t genome, void *context)
+{
+  double fitness;
+  unsigned long total = 0;
+  unsigned short i;
+  unsigned short *numbers = context;
+  unsigned short total_numbers = 0;
+
+  for (i = 0; i < IH_GENOME_SIZE_BITS; i++) {
+    if (ih_bit_get(genome, i)) {
+      total += *(numbers + i);
+      total_numbers++;
+    }
+  }
+
+  /*  fitness = 1 / (1 + fabs(GOAL_TOTAL - total));  */
+  fitness = (1 / (1 + fabs(GOAL_TOTAL - total))) / (1 + total_numbers);
+
+  return fitness;
+}
+
+int main(int argc, char *argv[])
+{
+  ih_genome_t fittest_genome;
+  unsigned short i;
+  unsigned long total = 0;
+  ih_boole_t first_number = ih_boole_true;
+  ih_genetic_t *genetic;
+  double fittest_fitness;
+
+  genetic = ih_genetic_create(calculate_fitness, numbers, NULL);
+
+  ih_genetic_evolve(genetic, 0.1, &fittest_genome, &fittest_fitness);
+
+  for (i = 0; i < IH_GENOME_SIZE_BITS; i++) {
+    if (ih_bit_get(fittest_genome, i)) {
+      if (!first_number) {
+        printf(" + ");
+      }
+      total += *(numbers + i);
+      printf("%d", *(numbers + i));
+      first_number = ih_boole_false;
+    }
+  }
+  printf(" = %lu\n", total);
+
+  ih_genetic_destroy(genetic);
+
+  return 0;
+}
